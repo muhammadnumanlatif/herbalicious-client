5a31c7b4650d5acf2e359be9979f24470746a3c6
muhammadnumanlatif <muhammadnumanlatif@gmail.com>
2026-02-14 03:30:56 +0500

    Fix: Remove FloatingWhatsApp and update BackToTop icon/position

--- app/layout.tsx
+++ app/layout.tsx
@@ -100,7 +100,6 @@ export default function RootLayout({
                         <SocialProofToast />
                         <ExitIntentModal />
                         <MobileBottomNav />
-                        <FloatingWhatsApp />
                         <FloatingBar />
                         <BackToTop />
                         <CommandPalette />
