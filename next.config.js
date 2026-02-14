02f97793efa2b684645de38daca513c61095606f
muhammadnumanlatif <muhammadnumanlatif@gmail.com>
2026-02-14 16:27:32 +0500

    Perf: Enable Next.js AVIF/WebP image optimization and add remotePatterns

--- next.config.js
+++ next.config.js
@@ -2,7 +2,18 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
+        formats: ['image/avif', 'image/webp'],
         remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'cms.herbalicious-shop.com',
+                pathname: '/wp-content/uploads/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'herbalicious-wp.local',
+                pathname: '/wp-content/uploads/**',
+            },
             {
                 protocol: 'https',
                 hostname: '**',
